4afb88049896d76bb1df691162762b85fe65e91a
Mgauth <37074876+Mgauth@users.noreply.github.com>
2020-12-01 02:26:27 +0100

    Add files via upload

---
+++ R/test_perm.R
@@ -0,0 +1,109 @@
+#' Permutation test using the test statistic
+#'
+#' @param Y
+#' @param X
+#' @param Z
+#' @param n_perm
+#' @param parallel
+#' @param n_cpus
+#'
+#'
+#' @export
+
+
+
+test_perm <- function(Y, X, Z=NULL, n_perm=100, parallel = TRUE, n_cpus = NULL){
+
+  if(parallel){
+    if(is.null(n_cpus)){
+      n_cpus <- parallel::detectCores() - 1
+    }
+    cl <- parallel::makeCluster(n_cpus)
+    doParallel::registerDoParallel(cl)
+  }else{
+    cl <- NULL
+  }
+
+  y <- sort(unique(Y))
+
+  if (is.null(Z)){
+    #modelmat <- model.matrix(Y~X)
+    modelmat <- cbind(rep(1,length(Y)),X)
+  }
+  else{
+    modelmat <- model.matrix(Y~X+Z)
+  }
+
+  beta <- rep(NA,length(y))
+  for (i in 1:length(y)){
+    indi_Y <- 1*(Y<=y[i])
+    reg <- lm(indi_Y ~ 1 + modelmat[,-1])
+    beta[i] <- reg$coefficients[2]
+  }
+
+  z <- sqrt(length(y)-1)*(beta[-length(y)])
+  STAT_obs <- sum(t(z)*z)
+
+  STAT_perm <- rep(NA,n_perm)
+
+  if (is.null(Z)){
+
+    for(k in 1:n_perm){
+
+      X_star <- sample(X)
+      modelmat_perm <- model.matrix(Y~X_star)
+      beta_perm<- rep(NA,length(y))
+
+      for (i in 1:length(y)){
+        indi_Y <- 1*(Y<=y[i])
+        reg <- lm(indi_Y ~ 1 + modelmat_perm[,-1])
+        beta_perm[i] <- reg$coefficients[2]
+      }
+
+      z <- sqrt(length(y)-1)*(beta_perm[-length(y)])
+      STAT_perm[k] <- sum(t(z)*z)
+
+    }
+  }
+
+  else{
+
+    for (k in 1:n_perm){
+
+      sample_X <- function(X,Z,z){
+        X_sample <- rep(NA,length(Z))
+        for (i in 1:length(z)){
+          X_sample[which(Z==z[i])] <- sample(X[which(Z==z[i])])
+        }
+        return(X_sample)
+      }
+
+      X_star <- switch(class(Z),
+                       "factor" = sample_X(X,Z,unique(Z)),
+                       "numeric" = perm_cont(Y,X,Z))
+
+      modelmat_perm <- model.matrix(Y~X_star+Z)
+
+      for (i in 1:length(y)){
+        indi_Y <- 1*(Y<=y[i])
+        reg <- lm(indi_Y ~ 1 + modelmat_perm[,-1])
+        beta_perm[i] <- reg$coefficients[2]
+      }
+
+      z <- sqrt(length(y)-1)*(beta_perm[-length(y)])
+      STAT_perm[k] <- sum(t(z)*z)
+    }
+  }
+
+  if(parallel){
+    parallel::stopCluster(cl)
+  }
+
+  pval <- (sum(1*(STAT_perm>=STAT_obs))+1)/(n_perm+1)
+
+
+  return(data.frame(raw_pval=pval))
+
+}
+
+
