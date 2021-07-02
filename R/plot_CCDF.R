32952835f96fa98f84310ee402d6aab5bbf8941b
Marine Gauthier <marine.gauthier@u-bordeaux.fr>
2021-07-03 00:36:54 +0200

    plot

--- R/plot_CCDF.R
+++ R/plot_CCDF.R
@@ -46,27 +46,27 @@ plot_CCDF <- function(Y,X,Z=NULL,method="linear regression", fast=TRUE, space_y=
   res <- CCDF(as.numeric(Y[,1]),X,Z,method,fast,space_y,number_y)
   
   
-  if (class(Z)=="NULL"){
+  if (class(Z[,1])=="NULL"){
     
     df_plot <- data.frame("y" = res$y, "x" = res$x, "cdf" = res$cdf, "ccdf" = res$ccdf)
     
     if (is.factor(X[,1])){
       l_X <- length(unique(X[,1]))
       ggplot() + ggtitle(colnames(Y)) +
-        geom_step(data = df_plot,aes(x = y, y = cdf, color = viridis(n=(l_X+1))[1]), size = 1, linetype="dotted") +
+        geom_step(data = df_plot,aes(x = y, y = cdf, color = viridis(n=(l_X+1))[1]), size = 1, linetype="solid") +
         geom_step(data = df_plot, aes(x = y, y = ccdf, color = as.factor(x)), size = 1) +
         scale_color_manual(name = "", labels = c("CDF", paste0("CCDF X=", unique(df_plot$x)), "CCDF2"),
                            values = c(viridis(n=(l_X+1))[1],viridis(n=(l_X+1))[-1]),
-                           guide = guide_legend(override.aes = list(linetype = c("dotted",rep("solid",l_X))))) + xlab("y") +
+                           guide = guide_legend(override.aes = list(linetype = c("solid",rep("solid",l_X))))) + xlab("y") +
         ylab("value") + theme_bw() + theme(plot.title = element_text(hjust = 0.5))
     }
     else{
       ggplot() + ggtitle(colnames(Y)) +
-        geom_step(data = df_plot,aes(x = y, y = cdf, color = viridis(n=2)[1]), size = 1, linetype="dotted") +
+        geom_step(data = df_plot,aes(x = y, y = cdf, color = viridis(n=2)[1]), size = 1, linetype="solid") +
         geom_point(data = df_plot, aes(x = y, y = ccdf, color =  viridis(n=2)[2]), size = 1) +
         scale_color_manual(name = "", labels = c("CDF", "CCDF"),
                            values = c(viridis(n=3)),
-                           guide = guide_legend(override.aes = list(linetype = c("dotted","solid"),
+                           guide = guide_legend(override.aes = list(linetype = c("solid","solid"),
                                                                     shape = c(NA,16)))) + xlab("y") +
         ylab("value") + theme_bw() + theme(plot.title = element_text(hjust = 0.5))
     }
@@ -75,41 +75,39 @@ plot_CCDF <- function(Y,X,Z=NULL,method="linear regression", fast=TRUE, space_y=
   
   else{
     
+    res <- CCDF(as.numeric(Y[,1]),X,Z,method,fast,space_y,number_y)
+    
     df_plot <- data.frame("y" = res$y, "x" = res$x, "z" = res$z, "cdf" = res$cdf, "ccdf_nox" = res$ccdf_nox,  "ccdf_x" = res$ccdf_x)
     
     if (is.factor(X[,1])){
       l_X <- length(unique(X[,1]))
       ggplot() + ggtitle(colnames(Y)) +
-        geom_step(data = df_plot, aes(x = y, y = cdf, color = viridis(n=(l_X+2))[1]), size = 1, linetype="dotted") +
-        geom_point(data = df_plot, aes(x = y, y = ccdf_x, color = as.factor(x)), shape=16, size = 2) +
-        geom_point(data = df_plot, aes(x = y, y = ccdf_nox, color="black"), shape=2, size = 2) +
+        geom_step(data = df_plot, aes(x = y, y = cdf, color = viridis(n=(l_X+2))[1]), size = 1, linetype="solid") +
+        geom_point(data = df_plot, aes(x = y, y = ccdf_x, color = as.factor(x)), shape=16, size = 1) +
+        geom_point(data = df_plot, aes(x = y, y = ccdf_nox, color="black"), shape=2, size = 1) +
         scale_color_manual(name = "", 
                            labels = c("CDF", paste0("CCDF_X X=", unique(df_plot$x)), "CCDF_noX"),
                            values = c(viridis(n=(l_X+2))[-(l_X+2)],"gold"),
-                           guide = guide_legend(override.aes = list(linetype = c("dotted",rep("solid",l_X),"solid"),
+                           guide = guide_legend(override.aes = list(linetype = c("solid",rep("solid",l_X),"solid"),
                                                                     shape = c(NA,rep(16,l_X),2)))) +
         xlab("y") +
         ylab("value") + theme_bw() + theme(plot.title = element_text(hjust = 0.5))
-
-      
       
     }
+    
     else{
       ggplot() + ggtitle(colnames(Y)) +
-        geom_step(data = df_plot, aes(x = y, y = cdf, color = viridis(n=3)[1]), size = 1, linetype="dotted") +
+        geom_step(data = df_plot, aes(x = y, y = cdf, color = viridis(n=3)[1]), size = 1, linetype="solid") +
         geom_point(data = df_plot, aes(x = y, y = ccdf_x, color = viridis(n=3)[2]), shape=16, size = 2) +
         geom_point(data = df_plot, aes(x = y, y = ccdf_nox, color=viridis(n=3)[3]), shape=2, size = 2) +
         scale_color_manual(name = "", 
                            labels = c("CDF", "CCDF_X", "CCDF_noX"),
                            values = c(viridis(n=3)),
-                           guide = guide_legend(override.aes = list(linetype = c("dotted","solid","solid"),
+                           guide = guide_legend(override.aes = list(linetype = c("solid","solid","solid"),
                                                                     shape = c(NA,16,2)))) +
         xlab("y") +
         ylab("value") + theme_bw() + theme(plot.title = element_text(hjust = 0.5))
     }
-    
-    plot(res$y,res$ccdf_nox)
-    #return(list(cdf=cdf, ccdf_nox=ccdf_nox, ccdf_x=ccdf_x, y=y_sort, x=x_sort, z=z_sort))
   }
 }
 
