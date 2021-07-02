2d2e6c62360664825eb2f99be2f84fb6050882cc
Marine Gauthier <marine.gauthier@u-bordeaux.fr>
2021-07-02 21:29:57 +0200

    linetype

--- R/plot_CCDF.R
+++ R/plot_CCDF.R
@@ -53,18 +53,21 @@ plot_CCDF <- function(Y,X,Z=NULL,method="linear regression", fast=TRUE, space_y=
     if (is.factor(X[,1])){
       l_X <- length(unique(X[,1]))
       ggplot() + ggtitle(colnames(Y)) +
-        geom_step(data = df_plot,aes(x = y, y = cdf, color = viridis(n=(l_X+1))[1]), size = 1) +
+        geom_step(data = df_plot,aes(x = y, y = cdf, color = viridis(n=(l_X+1))[1]), size = 1, linetype="dotted") +
         geom_step(data = df_plot, aes(x = y, y = ccdf, color = as.factor(x)), size = 1) +
         scale_color_manual(name = "", labels = c("CDF", paste0("CCDF X=", unique(df_plot$x)), "CCDF2"),
-                           values = c(viridis(n=(l_X+1))[1],viridis(n=(l_X+1))[-1])) + xlab("y") +
+                           values = c(viridis(n=(l_X+1))[1],viridis(n=(l_X+1))[-1]),
+                           guide = guide_legend(override.aes = list(linetype = c("dotted",rep("solid",l_X))))) + xlab("y") +
         ylab("value") + theme_bw() + theme(plot.title = element_text(hjust = 0.5))
     }
     else{
       ggplot() + ggtitle(colnames(Y)) +
-        geom_step(data = df_plot,aes(x = y, y = cdf, color = viridis(n=2)[1]), size = 1) +
+        geom_step(data = df_plot,aes(x = y, y = cdf, color = viridis(n=2)[1]), size = 1, linetype="dotted") +
         geom_point(data = df_plot, aes(x = y, y = ccdf, color =  viridis(n=2)[2]), size = 1) +
         scale_color_manual(name = "", labels = c("CDF", "CCDF"),
-                           values = c(viridis(n=3))) + xlab("y") +
+                           values = c(viridis(n=3)),
+                           guide = guide_legend(override.aes = list(linetype = c("dotted","solid"),
+                                                                    shape = c(NA,16)))) + xlab("y") +
         ylab("value") + theme_bw() + theme(plot.title = element_text(hjust = 0.5))
     }
   }
@@ -77,14 +80,14 @@ plot_CCDF <- function(Y,X,Z=NULL,method="linear regression", fast=TRUE, space_y=
     if (is.factor(X[,1])){
       l_X <- length(unique(X[,1]))
       ggplot() + ggtitle(colnames(Y)) +
-        geom_step(data = df_plot, aes(x = y, y = cdf, color = viridis(n=(l_X+2))[1]), size = 1) +
+        geom_step(data = df_plot, aes(x = y, y = cdf, color = viridis(n=(l_X+2))[1]), size = 1, linetype="dotted") +
         geom_point(data = df_plot, aes(x = y, y = ccdf_x, color = as.factor(x)), shape=16, size = 2) +
         geom_point(data = df_plot, aes(x = y, y = ccdf_nox, color="black"), shape=2, size = 2) +
         scale_color_manual(name = "", 
                            labels = c("CDF", paste0("CCDF_X X=", unique(df_plot$x)), "CCDF_noX"),
                            values = c(viridis(n=(l_X+2))[-(l_X+2)],"gold"),
-                           guide = guide_legend(override.aes = list(linetype = c(1,0,0,0),
-                                                                    shape = c(NA,16,16,2)))) +
+                           guide = guide_legend(override.aes = list(linetype = c("dotted",rep("solid",l_X),"solid"),
+                                                                    shape = c(NA,rep(16,l_X),2)))) +
         xlab("y") +
         ylab("value") + theme_bw() + theme(plot.title = element_text(hjust = 0.5))
 
@@ -93,13 +96,13 @@ plot_CCDF <- function(Y,X,Z=NULL,method="linear regression", fast=TRUE, space_y=
     }
     else{
       ggplot() + ggtitle(colnames(Y)) +
-        geom_step(data = df_plot, aes(x = y, y = cdf, color = viridis(n=3)[1]), size = 1) +
+        geom_step(data = df_plot, aes(x = y, y = cdf, color = viridis(n=3)[1]), size = 1, linetype="dotted") +
         geom_point(data = df_plot, aes(x = y, y = ccdf_x, color = viridis(n=3)[2]), shape=16, size = 2) +
         geom_point(data = df_plot, aes(x = y, y = ccdf_nox, color=viridis(n=3)[3]), shape=2, size = 2) +
         scale_color_manual(name = "", 
                            labels = c("CDF", "CCDF_X", "CCDF_noX"),
                            values = c(viridis(n=3)),
-                           guide = guide_legend(override.aes = list(linetype = c(1,0,0),
+                           guide = guide_legend(override.aes = list(linetype = c("dotted","solid","solid"),
                                                                     shape = c(NA,16,2)))) +
         xlab("y") +
         ylab("value") + theme_bw() + theme(plot.title = element_text(hjust = 0.5))
